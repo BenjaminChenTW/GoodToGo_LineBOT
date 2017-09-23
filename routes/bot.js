da1225cf4878fc3b35503eabc7fc206d12328c36
Peter <a0917209079@gmail.com>
2017-09-23 20:09:45 +0800

    let bot can receive image and put event into messageObj

--- routes/bot.js
+++ routes/bot.js
@@ -5,14 +5,17 @@ var config = require('../config/config.js');
 const Client = require('@line/bot-sdk').Client;
 const client = new Client(config.bot);
 
+var messageObj = {}
+
 module.exports = {
     // event handler
     handleEvent: function(event) {
-        if (event.type !== 'message' || event.message.type !== 'text') {
+        if (event.type !== 'message' || event.message.type !== 'text' || event.message.type !== 'image') {
             // ignore non-text-message event
             return Promise.resolve(null);
         }
 
+        messageObj = event
         // create a echoing text message
         const echo = { type: 'text', text: event.message.text };
 
