f35fdb234d70e9034de597587518ba87dc336550
Benjamin Chen <s910190benjamin@gmail.com>
2017-10-03 15:04:30 +0800

    finish lottery, fix img emit

--- routes/lottery.js
+++ routes/lottery.js
@@ -1,31 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var debug = require('debug')('goodtogo-linebot:lottery');
 
+var Coupon = require('../models/DB/couponDB.js');
 var templateSendler = require('../models/messageProcess.js').templateSendler;
-// check undefined
+
 router.get('/draw/:id', function(req, res, next) {
     var id = req.params.id;
-    // 抽獎券DB
-    res.render('draw', {
-        amount: 0,
-        couponList: []
+    if (id === 'undefined') return next();
+    Coupon.find({ 'userId': id, 'read': false }, function(err, coupons) {
+        var couponList = [];
+        for (var i = 0; i < coupons.length; i++) {
+            couponList.push({
+                id: coupons[i].couponId,
+                refImgIndex: picIndex
+            });
+        }
+        res.render('draw', {
+            amount: coupons.length,
+            couponList: couponList
+        });
     });
 });
 
-router.get('/sendcoupon/:id', function(req, res, next) {
-    var id = req.params.id;
-    // 抽獎券DB
-    templateSendler(lineUserId, function() {
-        res.status(200).json({});
-    }, isWin, couponId, couponType, couponContent);
+router.get('/sendcoupon/:couponId', function(req, res, next) {
+    var couponId = req.params.couponId;
+    if (couponId === 'undefined') return res.status(404).end();
+    Coupon.findOne({ 'couponId': couponId, 'read': false }, function(err, coupon) {
+        if (!coupon) return res.status(404).end();
+        coupon.read = true;
+        coupon.save(function(err) {
+            if (err) return debug(JSON.stringify(err));
+            templateSendler(coupon.userId, function() {
+                res.status(200).json({
+                    couponId: couponId,
+                    isWin: coupon.isWin,
+                    prizeType: coupon.prizeType,
+                    prizeName: coupon.prizeName,
+                });
+            }, coupon.isWin, couponId, coupon.prizeType, coupon.prizeName);
+        });
+    });
 });
 
-router.get('/exchange/:id', function(req, res, next) {
-    var id = req.params.id;
-    // 抽獎券DB set exchange
-    res.render('exchange', {
-        couponId: 0, // couponId
-        couponContent: '' //ouponContent
+router.get('/exchange/:couponId', function(req, res, next) {
+    var couponId = req.params.couponId;
+    if (couponId === 'undefined') return next();
+    Coupon.findOne({ 'couponId': couponId, 'read': true, 'isWin': true }, function(err, coupon) {
+        if (!coupon) return next();
+        res.render('exchange', {
+            couponId: couponId,
+            type: coupon.prizeType,
+            couponContent: coupon.prizeName
+        });
     });
 });
 
