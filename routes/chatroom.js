edf69439f243b6e25586915e10c8cb649034077f
Benjamin Chen <s910190benjamin@gmail.com>
2017-10-03 14:04:39 +0800

    add session control

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -15,7 +15,8 @@ router.get('/', function(req, res, next) {
             next(err);
         } else {
             res.render('chatroom', {
-                'roomList': roomList
+                'roomList': roomList,
+                'lastRoom': req.session.userId || 'none'
             });
         }
     });
@@ -24,6 +25,7 @@ router.get('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
     var id = req.params.id;
     if (id === 'undefined') return res.status(404).end();
+    req.session.userId = id;
     getMessage(id, next, function(isEmpty, messageList) {
         res.json({
             'isEmpty': isEmpty,
