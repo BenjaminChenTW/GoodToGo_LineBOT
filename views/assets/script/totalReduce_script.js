919e17545e3cffd2625cc0f8bc3e6bbb2dda0c7b
mill010363 <jack98526@gmail.com>
2017-10-05 13:54:50 +0800

    new

--- views/assets/script/totalReduce_script.js
+++
@@ -1,6 +0,0 @@
-if (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase())) {
-    console.log("in phone")
-} else {
-    $("#css_style").attr("href","assets/css/totalReduce.css"); 
-    console.log("in web");
-}
